b158be682ed90a986a611f19a1bed3b373264350
zilani131 <zilani.cuet@gmail.com>
2022-05-24 15:41:27 +0600

    purchase page api created and set the data to the database

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ async function run(){
     try{
         await client.connect();
         const toolsCollection=client.db("manufacture").collection("tools");
+        const profilesCollection=client.db("manufacture").collection("user");
         // tools collection
         app.get('/tools',async (req,res)=>{
             const query={};
@@ -28,9 +29,50 @@ async function run(){
         // purchase tool api
         app.get('/purchase/:_id',async (req,res)=>{
             const _id=req.params._id;
+         
             const query={_id:ObjectId(_id)}
             const tool=await toolsCollection.findOne(query)
             res.send(tool)
+        })
+        // update quantity
+        app.put('/purchase/:_id',async(req,res)=>{
+            const _id=req.params._id;
+            const newQuantity = req.body.newQuantity;
+            console.log(newQuantity)
+            const filter={_id:ObjectId(_id)};
+            const options = { upsert: true };
+            const updateDoc={
+                $set: {
+                    quantity:newQuantity,
+                  },
+            }
+            const result = await toolsCollection.updateOne(filter, updateDoc, options);
+            res.send(result)
+        })
+        // user information 
+        app.post('/user',async (req,res)=>{
+            const email =req.query.email;
+            const profile=req.body;
+            console.log(email,profile)
+            const result = await profilesCollection.insertOne(profile);
+            res.send(result)
+        })
+        // user information update
+        app.put('/user',async(req,res)=>{
+            const email=req.query.email;
+            const address=req.body.address;
+            const phone=req.body.phone;
+            console.log
+            const filter ={email:email};
+            const options={upsert:true};
+            const updateDoc={
+                $set: {
+                    address:address,
+                    phone:phone
+                  }
+            }
+            const result = await profilesCollection.updateOne(filter, updateDoc, options);
+            res.send(result)
         })
     }
     finally{
